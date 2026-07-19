61e97028b347e6dc8bb53f83aaf6a2d1292be662
Khongchai <world1955@hotmail.com>
2026-07-19 12:11:30 +0700

    updated script

--- wasm-compile-debug.sh
+++ wasm-compile-debug.sh
@@ -5,5 +5,5 @@ emcc -O0 -g opusenc_extension.c .libs/libopusenc.a ../opus/.libs/libopus.a \
   -s STACK_SIZE=1048576 -s STACK_OVERFLOW_CHECK=2 \
   -s MODULARIZE=1 -s EXPORT_NAME="createOpusEncModule" \
   -s EXPORTED_FUNCTIONS="['_ope_comments_create', '_ope_comments_add', '_ope_comments_destroy', '_ope_encoder_create_pull', '_ope_encoder_write_float', '_ope_encoder_get_page', '_ope_encoder_drain', '_ope_encoder_destroy', '_ope_strerror', '_variation_ope_encoder_ctl_set_int', '_variation_ope_encoder_ctl_get_int', '_malloc', '_free']" \
-  -s EXPORTED_RUNTIME_METHODS=ccall,cwrap,HEAPF32,HEAPU8,HEAP16,HEAP32,UTF8ToString \
+  -s EXPORTED_RUNTIME_METHODS=ccall,cwrap,HEAPF32,HEAPU8,HEAP16,HEAP32 \
   -o opusenc.js
